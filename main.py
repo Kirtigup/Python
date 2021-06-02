416b55ebb5a3cc8aedfa0a076aa4ed3bec0f069e
Samrika Singh <67017620+samrika25@users.noreply.github.com>
2021-06-02 15:43:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import os
 #flask package install (in that Flask class define)((Flask function from flask package)render_template=used to generate output from template file based on jinja2 engine that is found in application's template folder )
 from flask import Flask, render_template
 #app defined here 
 app = Flask(__name__)
+color=os.environ.get('APP_COLOR')
 #whenever end user goes in /point he or she will see output
 @app.route('/')
 def hello_world():
-    return render_template('index.html');
+    print(color)
+    return render_template('index.html',color=color);
 #here app.run is used to run ur web based application
 if __name__ == "__main__":
     app.run(host ='0.0.0.0', port=5001 ,debug =True)
