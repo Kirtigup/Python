48236097a6acd21e6842dd4efa42155a5d4324b4
Samrika Singh <67017620+samrika25@users.noreply.github.com>
2021-07-02 14:14:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     print(color)
-    return render_template('index.html',color=color);
+    return render_template('index.html');
 #here app.run is used to run ur web based application
 if __name__ == "__main__":
     app.run(host ='0.0.0.0', port=5001 ,debug =True)
